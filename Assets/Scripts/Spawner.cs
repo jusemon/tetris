9d9c6d8d01951741f44236a18d8759756111d409
Juan Sebastián Montoya Montoya <juansmm@outlook.com>
2019-11-14 11:44:10 -0500

    Added colors

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,15 +7,25 @@ public class Spawner : MonoBehaviour
     // Groups
     public GameObject[] groups;
 
+    public Color[] colors;
+
     public void spawnNext()
     {
         // Random Index
         int i = Random.Range(0, groups.Length);
 
         // Spawn Group at current Position
-        Instantiate(groups[i],
+        var instance = Instantiate(groups[i],
                     transform.position,
                     Quaternion.identity);
+
+        // Setting instance colors
+        var components = instance.gameObject.GetComponentsInChildren<SpriteRenderer>();
+        var color = colors[i];
+        foreach (var item in components)
+        {
+            item.color = new Color(color.r, color.g, color.b);
+        }
     }
 
     // Start is called before the first frame update
